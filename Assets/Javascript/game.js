74d8a151fb20c34dc2c6b0608a46ed5d29ed85a3
Chase Cannon <chase27cannon52@gmail.com>
2019-08-10 20:52:58 -0500

    second commit

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,51 @@
+//create an array of words
+const word = ['Michael Scott','Jim','Dwight','Pam','Creed','Toby', 'Daryl', 'Stanley','Kevin' ]
+//choose word randomly
+let randNum = Math.floor (Math.random() * word.length);
+let chosenWord = word[randNum];
+let rightWord = [];
+let wrongWord = [];
+let underScore = [];
+console.log(chosenWord);
+//Dom manipulation
+let docUnderScore = document.getElementsByClassName('underscore');
+let docRightGuess = document.getElementsByClassName('rightGuess');
+let docWrongGuess = document.getElementsByClassName('wrongGuess');
+
+console.log(chosenWord);
+//create underscores based on length of word
+let generateUnderscore = () => {
+    for(let i = 0; i < chosenWord.length; i++) {
+        underScore.push('_')
+    }
+    return underScore;
+}
+console.log(generateUnderscore());
+//get users guess
+document.addEventListener('keypress', (event)=>{
+    let keyword = String.fromCharCode(event.keycode);
+//if user guesses right
+    if(chosenWord.indexOf(keyword) > -1){
+        console.log(true);
+    }
+    //add to right words array
+    rightWord.push(keyword);
+    docUnderScore[0].innerHTML = generateunderScore.join('');
+    rightGuess[0].innerHTML = rightGuess.join(''); 
+        console.log(rightWord);
+    //replace undrtscore with right letter
+    underScore [(chosenWord.indexOf(keyword)) = keyword];
+        docUnderScore.innerHTML = underScore.join('');
+            console.log(underScore);
+       
+});
+//check if guess is right
+if(underScore.join('') == chosenWord){
+    alert('You Win')
+}
+else{ wrongWord.push(keyword);
+    wrongGuess[0].innerHTML = wrongGuess.join('');
+        console.log(wrongWord);
+}
+//if right push to right array
+//if wrong push to wrong array
